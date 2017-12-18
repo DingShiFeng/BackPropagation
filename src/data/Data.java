c4dc9ea661218fea90c5e4a937ba75e1c03c6c59
Shifeng Ding <30685657+DingShiFeng@users.noreply.github.com>
2017-12-18 17:07:06 +0800

    Update Data.java

--- src/data/Data.java
+++ src/data/Data.java
@@ -1,1 +1,22 @@
+package data;
 
+import java.util.ArrayList;
+
+public class Data {
+	private ArrayList<Double> attributionList = null;
+	
+	public Data() {
+		this.attributionList = new ArrayList<Double>();
+	}
+	
+	public void addAttribution(Double attribution) {
+		this.attributionList.add(attribution);
+	}
+	
+	public ArrayList<Double> getAttributionList() {
+		return attributionList;
+	}
+	public void setAttributionList(ArrayList<Double> attributionList) {
+		this.attributionList = attributionList;
+	}
+}
