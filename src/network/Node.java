368c2169c7e8e5103062c92f7b11a6d54de94252
Shifeng Ding <30685657+DingShiFeng@users.noreply.github.com>
2017-12-18 17:08:26 +0800

    Update Node.java

--- src/network/Node.java
+++ src/network/Node.java
@@ -1,1 +1,39 @@
+package network;
 
+public class Node {
+	private Layer associatedLayer = null;
+	
+	private double outputValue;
+	
+	private double outputError;
+	private double hiddenError;
+	
+	public Node(Layer associatedLayer) {
+		this.associatedLayer = associatedLayer;
+	}
+	
+	public Layer getAssociatedLayer() {
+		return associatedLayer;
+	}
+	public void setAssociatedLayer(Layer associatedLayer) {
+		this.associatedLayer = associatedLayer;
+	}
+	public double getOutputValue() {
+		return outputValue;
+	}
+	public void setOutputValue(double outputValue) {
+		this.outputValue = outputValue;
+	}
+	public double getOutputError() {
+		return outputError;
+	}
+	public void setOutputError(double outputError) {
+		this.outputError = outputError;
+	}
+	public double getHiddenError() {
+		return hiddenError;
+	}
+	public void setHiddenError(double hiddenError) {
+		this.hiddenError = hiddenError;
+	}
+}
