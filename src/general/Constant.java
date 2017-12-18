ee983cc4103f5a034f30ed8d878ebb5e83b0a4ff
Shifeng Ding <30685657+DingShiFeng@users.noreply.github.com>
2017-12-18 17:07:48 +0800

    Update Constant.java

--- src/general/Constant.java
+++ src/general/Constant.java
@@ -1,1 +1,14 @@
+package general;
 
+public class Constant {
+	public static final String dataFile = "letter-recognition.txt";
+	public static final String resultFile = "letter-recognition-result.txt";
+	
+	public static final int inputNodeNum = 16;
+	public static final int hiddenNodeNum = 20;
+	public static final int outputNodeNum = 26;
+	
+	public static final double eta = 0.01;
+	
+	public static final int COUNT = 20000;
+}
